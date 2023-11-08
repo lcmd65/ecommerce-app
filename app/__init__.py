7d174a57a32042f9a2cbe03f1c199be0e9c93e33
Dat <dat.lemindast@gmail.com>
2023-11-08 16:38:49 +0700

    change

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,11 @@
 from flask import Flask, blueprints
 from flask import Cache
+import gc
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    app.iter_blueprints()
+    
+    
+    return app
+    
