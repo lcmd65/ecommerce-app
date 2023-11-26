ed79280419aa5caf984c83b0b0bccdda63a5bd74
Dat <dat.lemindast@gmail.com>
2023-11-26 18:54:29 +0700

    update

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -2,15 +2,17 @@ function changeHeightClose() {
     const box = document.querySelector(".chat-box-container");
     // Check the current height and toggle between 400px and 25px
     // Check the current height and toggle between 400px and 25px
-    if (box.clientHeight === 400) {
-        box.style.height = "25px";
-    }
+    box.style.height = "25px";
 }
 
 function changeHeightOpen() {
     const box = document.querySelector(".chat-box-container");
     // Check the current height and toggle between 400px and 25px
-    if (box.clientHeight === 25) {
-        box.style.height = "400px";
-    }
+    box.style.height = "400px";
 }
+
+const clickHeightOpen = document.querySelector("chat-button-openbutton")
+const clickHeightClose = document.querySelector("chat-button-closebutton")
+
+clickHeightClose.addEventListener("click", changeHeightClose);
+clickHeightOpen.addEventListener("click", changeHeightOpen);
