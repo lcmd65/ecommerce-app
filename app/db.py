490655fc6af36f0cf1d8e7071ab488134b1e88bc
Dat <dat.lemindast@gmail.com>
2023-11-28 18:01:05 +0700

    change config

--- app/db.py
+++ app/db.py
@@ -4,7 +4,8 @@ import json
 def database_connection():
     with open("app/schema.json", "r") as file:
         config = json.load(file)
-        # Connect to MongoDB
+        
+    # Connect to MongoDB
     client = MongoClient(config["mongo_uri"])
     database = client["Datathon"]
     return client, database
