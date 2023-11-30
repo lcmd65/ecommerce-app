3411de524c218928d3618cb0b4441f20fa41744c
Dat <dat.lemindast@gmail.com>
2023-11-30 22:42:17 +0700

    update logic

--- app/api.py
+++ app/api.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, request, jsonify
 from app.db import database_connection
-import app.cache
-import json
 
 ## chat data store in cache
 ## chat_data = {id: str, chat: []}
@@ -85,64 +83,3 @@ def chat():
     message = request.json.get('message')
     respone = processing(message)
     return jsonify(respone)
-
-@api_blueprint.route("/description_get", methods = ['GET', 'POST'])
-def description_get():
-    """
-    get item product description view when click detail to product
-
-    Returns:
-        description: str
-    """
-    message = request.json.get('_id') # product id
-    client, database = database_connection()
-    collection = database["Product"]
-    item = collection.find_one({'_id': message})
-    client.close()
-    return item['description']
-
-@api_blueprint.route("/cart_get", methods = ['GET', 'POST'])
-def cart_get():
-    """
-    get the cart data when click cart icon button
-
-    Returns:
-        cart: [id: user_id, [cart_data: data]]
-    """
-    cart = app.cache.cache.get("cart")
-    if cart == None :
-        user = app.cache.cache.get("user")
-        user_id = user["id"]
-        client, database = database_connection()
-        collection = database["User_Card"]
-        cart = collection.find_one({'id': user_id})
-        client.close()
-    return jsonify(json.dumps(cart))
-
-@api_blueprint.route("/cart_add", methods = ['GET', 'POST'])
-def cart_add():
-    """
-    cart add one item when event clicked
-
-    Returns:
-        bool: result of event item add to db
-    """
-    try:
-        item_id = request.json.get('_id') 
-        user_id = app.cache.cache.get('user')['id']
-        client, database = database_connection()
-        collection = database["User_Card"]
-        cart_db = collection.find_one({'id': user_id})
-        items = json.loads(cart_db.get('item', '[]')).append(item_id)
-        collection.update_one({'id': user_id}, {'$set': {'item': json.dumps(items)}})
-        client.close()
-        return "True"
-    except:
-        return "False"
-    
-    
-
-    
-    
-
-    
--- app/blog/views/views.py
+++ app/blog/views/views.py
@@ -1,11 +1,12 @@
 #blogging main function of nohdata
-from flask import Blueprint
 from flask import (
+    Blueprint,
     request,
     render_template,
     redirect,
     jsonify,
     g)
+from app.db import database_connection
 import app.cache
 import json
 
@@ -16,13 +17,62 @@ blog_blueprint = Blueprint('blog_blueprint', __name__)
 def home():
     from app.auth.models.user import User
     user = json.loads(app.cache.cache.get('user')) # cache get
-    print(user)
-    user_model = User(user) # NULL data user
+    print(user) # NULL data user
     
     if request.method == "POST":
         render_template("blog/home.html" ,user_name = user["username"])
     return render_template("blog/home.html", user_name = user["username"])
 
+@blog_blueprint.route("/description_get", methods = ['GET', 'POST'])
+def description_get():
+    """
+    get item product description view when click detail to product
 
+    Returns:
+        description: str
+    """
+    message = request.json.get('_id') # product id
+    client, database = database_connection()
+    collection = database["Product"]
+    item = collection.find_one({'_id': message})
+    client.close()
+    return item['description']
 
+@blog_blueprint.route("/cart_get", methods = ['GET', 'POST'])
+def cart_get():
+    """
+    get the cart data when click cart icon button
 
+    Returns:
+        cart: [id: user_id, [cart_data: data]]
+    """
+    cart = app.cache.cache.get("cart")
+    if cart == None :
+        user = app.cache.cache.get("user")
+        user_id = user["id"]
+        client, database = database_connection()
+        collection = database["User_Card"]
+        cart = collection.find_one({'id': user_id})
+        client.close()
+    return jsonify(json.dumps(cart))
+
+@blog_blueprint.route("/cart_add", methods = ['GET', 'POST'])
+def cart_add():
+    """
+    cart add one item when event clicked
+
+    Returns:
+        bool: result of event item add to db
+    """
+    try:
+        item_id = request.json.get('_id') 
+        user_id = app.cache.cache.get('user')['id']
+        client, database = database_connection()
+        collection = database["User_Card"]
+        cart_db = collection.find_one({'id': user_id})
+        items = json.loads(cart_db.get('item', '[]')).append(item_id)
+        collection.update_one({'id': user_id}, {'$set': {'item': json.dumps(items)}})
+        client.close()
+        return "True"
+    except:
+        return "False"
