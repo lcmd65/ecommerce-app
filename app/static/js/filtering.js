f327c555dccfc02bbb99d2dfc5f356b29a894fcd
Dat <dat.lemindast@gmail.com>
2024-06-13 10:37:24 +0700

    update force from lcmd

--- app/static/js/filtering.js
+++ app/static/js/filtering.js
@@ -10,19 +10,39 @@ async function filterProducts() {
     checkboxes.forEach(function(checkbox) {
         var categoryName = checkbox.getAttribute('name');
         var checkboxValue = checkbox.value;
+        selectedValues.appendChild({categoryName, categoryValue});
+        //query filtering value 
         if (!selectedValues[categoryName]) {
             selectedValues[categoryName] = [];
         }
         selectedValues[categoryName].push(checkboxValue);
     });
 
+
     // Convert the object to a JSON string and store it in a hidden input field
     var selectedValuesInput = document.createElement('input');
+    selectedValuesInput.classList.appendChild("hidden-json");
     selectedValuesInput.type = 'hidden';
     selectedValuesInput.name = 'selectedValues';
     selectedValuesInput.value = JSON.stringify(selectedValues);
     form.appendChild(selectedValuesInput);
 
+    const filter_direct = await fetch('/filtering_redirect', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({selectedValues})
+    });
+
+    const filtering_const = await fetch('/filtering', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ item })
+    });
     // Submit the form
     form.submit();
 }
+
